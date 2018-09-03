e07b4f30a8aa5607803bb45cb1dcd1a805874d62
nickvledder <nick_vledder@hotmail.com>
2018-09-03 14:38:23 +0200

    Update client.js
    to see datetime of every message

--- js/client.js
+++ js/client.js
@@ -1,6 +1,7 @@
 (function() {
   
-  let username   
+  let username;
+  var datetime;   
   const chat = new Chat();
 
   if( !(localStorage['username']) ) {
@@ -18,15 +19,17 @@
     chat.getUpdate(function(data) {
       data = JSON.parse(data)["log"];
       $("#log").html("");
-      for(i = 0; i < data.length; i++) {
-        $('#log').append("<p>" +data[i]['author'] + "<br>" + data[i]['message'] + '<p>');
+      //for(i = 0; i < data.length; i++) {
+      for(i = data.length-1; i > -1; i--) { 
+        $('#log').append("<p>" + data[i]['author'] + data[i]['datetime'] + "<br>" + data[i]['message']  + '<p>');
       }
     });
   };
   setInterval(update, 1000);
 
   $("#send").on('click', function(e) {
-    chat.send(username, $("#chat").val());
+    datetime = new Date();
+    chat.send(username, $("#chat").val(), datetime.getFullYear() + '/' + datetime.getMonth() + '/' + datetime.getDate() + ' ' + datetime.getHours() +  ':' + datetime.getMinutes() +  ' ' + datetime.getSeconds() + 's');
   });
   
   $("#logout").on('click', function(e) {
@@ -35,6 +38,5 @@
   });
 
 })();
-
 
 
